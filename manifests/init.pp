7a6d1f825c24c4004bbd86f83f2f4bc8a0b15ab5
Jordi Prats <jprats@systemadmin.es>
2016-11-19 18:02:52 +0100

    base

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,7 @@ class amavis(
                             $manage_docker_service = true,
                             $service_ensure        = 'running',
                             $service_enable        = true,
+                            $install_clamav        = true,
                           ) inherits amavis::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,11 @@
 class amavis::install inherits amavis {
 
+  if($amavis::install_clamav)
+  {
+    class { 'clamav': }
+    class { 'clamav::freshclam': }
+  }
+
   if($amavis::manage_package)
   {
     package { $amavis::params::package_name:
