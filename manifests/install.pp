d616f8ed0a6587198b0d92eb8f340bfc92c271dc
Jordi Prats <jprats@systemadmin.es>
2016-12-03 11:50:48 +0100

    afegit clamav server

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,8 +2,9 @@ class amavis::install inherits amavis {
 
   if($amavis::install_clamav)
   {
-    class { 'clamav': }
-    class { 'clamav::freshclam': }
+    class { 'clamav': } ->
+    class { 'clamav::freshclam': } ->
+    class { 'clamav::server': }
   }
 
   if($amavis::manage_package)
